36d7a18b234c07b4956548f2b9d19922803b055b
Hanafi Othman <hanafiothman14@gmail.com>
2024-12-20 19:39:30 +0800

    Fix issue with button props

--- app/components/QsButton.tsx
+++ app/components/QsButton.tsx
@@ -5,7 +5,7 @@ interface QsButtonProps extends ButtonProps {
   isLoading?: boolean;
 }
 
-const QsButton = ({ children, onClick, className, isLoading, disabled }: QsButtonProps) => {
+const QsButton = ({ children, onClick, className, isLoading, disabled, ...rest }: QsButtonProps) => {
   const baseClasses = 'inline-flex rounded-md bg-primary-main px-lg py-xs text-center text-white data-[hover]:bg-primary-hover data-[disabled]:opacity-30';
 
   return (
@@ -13,6 +13,7 @@ const QsButton = ({ children, onClick, className, isLoading, disabled }: QsButto
       className={twMerge(baseClasses, className as ClassNameValue)}
       onClick={onClick}
       disabled={disabled || isLoading}
+      {...rest}
     >
       {children}
     </Button>
